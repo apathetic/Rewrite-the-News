ae187550e91e29127c97b5cca99fa3f893fd906a
apathetic <wes.hatch@gmail.com>
2011-11-10 00:18:23 -0500

    asdf

---
+++ public/js/rewrite.js
@@ -0,0 +1,39 @@
+/*
+	Functions to set up the Client, interact with Server
+*/
+function ReWrite() {
+
+	console.log('Initializing client connection...');
+
+	// $.getJSON('/config.json', function(config) {
+	// 	console.log('Parsing configuration data...');
+	// 	var response = $('#response');
+	// 	response.html(config.host);
+
+
+
+		var socket = io.connect('http://apathetic.no.de', { port:8080 });
+//		var socket = io.connect('http://localhost', {port:8080});
+
+		socket.on('connect', function() {
+			console.log('Connected to server.');
+			socket.send('Hi Server...');
+		});
+		socket.on('message', function(data){ 
+			console.log('Recieved a message!'); 
+			response.html(data);
+			socket.emit('my other event', { my: 'data' });
+		});
+		socket.on('disconnect', function() {
+			console.log('disconnected from server'); 
+		});
+
+
+		socket.on('news', function (data) {    console.log(data);  });
+
+
+	});
+
+
+}
+
